c21da9f8b396d116ec07a2018e40e7895506c0a4
Ana <angoglez@gmail.com>
2018-05-28 01:57:37 +0200

    Cambiado archivo Hola.c, ahora tiene vidilla y muestra ASCII Art. Closes IamMiguelAA/Entregable_SD#2

--- Hola.c
+++ Hola.c
@@ -1,1 +1,20 @@
-//Este es un archivo simple que no tiene codigo, solo tiene comentarios.
+//Ahora este archivo lo modificamos y sí que tendrá código:
+
+
+#include <stdio.h>
+
+int main(){
+
+printf("  _           _       _                     _ _          \n");
+printf("| |         | |     | |                   (_|_)          \n");
+printf("| |__   ___ | | __ _| |__   __ _ _ __ ___  _ _  ___  ___ \n");
+printf("| '_ \\ / _ \\| |/ _` | '_ \\ / _` | '_ ` _ \\| | |/ _ \\/ __|\n");
+printf("| | | | (_) | | (_| | | | | (_| | | | | | | | | (_) \\__ \\ \n");
+printf("|_| |_|\\___/|_|\\__,_|_| |_|\\__,_|_| |_| |_|_| |\\___/|___/\n");
+printf("                                           _/ |               \n");
+printf("                                          |__/                \n");
+
+
+  //system("pause");
+  return 0;
+}
