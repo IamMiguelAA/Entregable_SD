dcbc3bf19d63b4d51279ba894e1a7228122912c3
YisGG <jgalvez.jgg@gmail.com>
2018-05-27 13:38:14 +0200

    Se ha creado el archivo Integrantes_grupo.py y se ha incluido el primer miembro del grupo

---
+++ Integrantes_grupo.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Programa que imprime por pantalla el nombre de los integrantes de grupo que realiza el entregable
+
+###############################Constantes###############################
+integrante1 = "Galvez Guerrero, Jesus"
+
+print("El entregable de SD se ha realizado por el siguiente grupo: ")
+print(" - " + integrante1)
