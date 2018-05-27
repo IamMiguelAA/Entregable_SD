0c636b6f0313b2f89116b4f7dee47727af5ad5c0
YisGG <jgalvez.jgg@gmail.com>
2018-05-27 13:41:27 +0200

    Se ha incluido un nuevo integrante al archivo Integrantes_grupo.py

--- Integrantes_grupo.py
+++ Integrantes_grupo.py
@@ -2,7 +2,9 @@
 # Programa que imprime por pantalla el nombre de los integrantes de grupo que realiza el entregable
 
 ###############################Constantes###############################
-integrante1 = "Galvez Guerrero, Jesus"
+integrante1 = "Alvarez Garcia, Miguel Angel"
+integrante2 = "Galvez Guerrero, Jesus"
 
 print("El entregable de SD se ha realizado por el siguiente grupo: ")
 print(" - " + integrante1)
+print(" - " + integrante2)
